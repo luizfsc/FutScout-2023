d33482e603ca6f18ebf862f8557e675f94b56f07
Luiz Costa <luiz.felipesenac@hotmail.com>
2023-10-21 13:09:36 -0300

    add parte final

---
+++ FutScout-2023/Models/AppDbcontext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FutScout_2023.Models
+{
+    public class AppDbcontext : DbContext
+    {
+            public AppDbcontext(DbContextOptions<AppDbcontext> options ) : base(options) { }
+
+        public DbSet<Atacante> Atacantes { get; set; }
+    }
+}
---
+++ FutScout-2023/Models/Atacante.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FutScout_2023.Models
+{
+    [Table("Atacantes")]
+    public class Atacante
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Inserir o dado")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Inserir o dado")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "Inserir o dado")]
+        public int Clube { get; set; }
+
+        [Required(ErrorMessage = "Inserir o dado")]
+        public int Partidas2023 { get; set; }
+
+        [Required(ErrorMessage = "Inserir o dado")]
+        public int Gols2023 { get; set; }
+
+        [Required(ErrorMessage = "Inserir o dado")]
+        public int Assists2023 { get; set; }
+
+        [Required(ErrorMessage = "Inserir o dado")]
+        public int Partidas2022 { get; set; }
+
+        [Required(ErrorMessage = "Inserir o dado")]
+        public int Gols2022 { get; set; }
+
+        [Required(ErrorMessage = "Inserir o dado")]
+        public int Assists2022 { get; set; }
+
+        public int Partidas2021 { get; set; }
+
+        public int Gols2021 { get; set; }
+
+        public int Assists2021 { get; set; }
+
+    }
+}
