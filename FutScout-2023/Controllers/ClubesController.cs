e1f27ff9f9b5ee2f9d589442456b913b5260a733
luizfsc <127213695+luizfsc@users.noreply.github.com>
2023-11-17 17:16:04 -0300

    Update ClubesController.cs

--- FutScout-2023/Controllers/ClubesController.cs
+++ FutScout-2023/Controllers/ClubesController.cs
@@ -1,11 +1,4 @@
-﻿using FutScout_2023.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
-namespace FutScout_2023.Controllers
-{
- 
-        public class ClubesController : Controller
+﻿        public class ClubesController : Controller
         {
             private readonly AppDbcontext _context;
             public ClubesController(AppDbcontext context)
@@ -13,13 +6,88 @@ namespace FutScout_2023.Controllers
                 _context = context;
             }
 
-            public async Task<IActionResult> Index()
-            {
-                var dados = await _context.Clubes.ToListAsync();
+        public async Task<IActionResult> Index(string sortBy, string sortDirection, string série)
+        {
+            var dados = await _context.Clubes.ToListAsync();
 
-                return View(dados);
+            // Realize a classificação dos dados com base no valor de sortBy.
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                dados = SortData(dados, sortBy, sortDirection);
             }
-            public IActionResult Create()
+
+            // Obtenha a lista completa de séries antes de aplicar o filtro
+            var todasAsSéries = dados.Select(x => x.Série).Distinct().ToList();
+
+            // Filtrar por Série, se especificado
+            if (!string.IsNullOrEmpty(série))
+            {
+                dados = dados.Where(x => x.Série == série).ToList();
+            }
+
+            // Adicione as séries no ViewBag para serem acessadas na view
+            ViewBag.TodasAsSéries = todasAsSéries;
+            ViewBag.SérieSelecionada = série;
+
+            return View(dados);
+        }
+
+
+        private List<Clube> SortData(List<Clube> data, string sortBy, string sortDirection)
+        {
+            if (sortDirection == "asc")
+            {
+                switch (sortBy)
+                {
+                    case "Nome":
+                        return data.OrderBy(item => item.Nome).ToList();
+                    case "Estado":
+                        return data.OrderBy(item => item.Estado).ToList();
+                    case "Série":
+                        return data.OrderBy(item => item.Série).ToList();
+
+                    case "gols2021":
+                        return data.OrderBy(item => item.gols2021).ToList();
+                    case "gols2022":
+                        return data.OrderBy(item => item.gols2022).ToList();
+                    case "gols2023":
+                        return data.OrderBy(item => item.gols2023).ToList();
+                    default:
+                        return data;
+                }
+            }
+
+            else if (sortDirection == "desc")
+            {
+                switch (sortBy)
+                {
+                    case "Nome":
+                        return data.OrderByDescending(item => item.Nome).ToList();
+                    case "Estado":
+                        return data.OrderByDescending(item => item.Estado).ToList();
+                    case "Série":
+                        return data.OrderByDescending(item => item.Série).ToList();
+
+                    case "gols2023":
+                        return data.OrderByDescending(item => item.gols2023).ToList();
+                    case "gols2022":
+                        return data.OrderByDescending(item => item.gols2022).ToList();
+                    case "gols2021":
+                        return data.OrderByDescending(item => item.gols2021).ToList();
+
+                    default:
+                        return data;
+                }
+            }
+
+            else
+            {
+                return data;
+            }
+        }
+
+
+        public IActionResult Create()
             {
                 return View();
             }
@@ -51,25 +119,24 @@ namespace FutScout_2023.Controllers
                 return View(dados);
             }
 
-            [HttpPost]
-            public async Task<IActionResult> Edit(int id, Clube clube)
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Clube clube)
+        {
+            if (id != clube.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
             {
-                if (id != clube.Id)
-                    return NotFound();
-
-                if (ModelState.IsValid)
-                {
-                    _context.Clubes.Update(clube);
-
-                    _context.Clubes.Add(clube);
-                    await _context.SaveChangesAsync();
-                    return RedirectToAction("Index");
-                }
-
-                return View();
+                _context.Clubes.Update(clube); 
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
             }
 
-            public async Task<IActionResult> Details(int? id)
+            return View(clube); 
+        }
+
+
+        public async Task<IActionResult> Details(int? id)
             {
                 if (id == null)
                     return NotFound();
@@ -114,5 +181,15 @@ namespace FutScout_2023.Controllers
 
                 return RedirectToAction("Index");
             }
+        private object GetPropertyValueByColumnName(Atacante item, string columnName)
+        {
+            var prop = typeof(Atacante).GetProperty(columnName);
+            if (prop != null)
+            {
+                return prop.GetValue(item, null);
+            }
+            return null;
         }
+    }
 }
+
